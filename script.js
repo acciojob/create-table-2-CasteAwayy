cf1498630aa9f59a40b1491d394735ed9bc0bb38
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-04 17:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function createTable() {
-    //Write your code here
-  
+const table = document.querySelector('#myTable');
+
+function createTable()
+{
+  const noOfRows =prompt('Iput number of rows');
+  const noOfCols =prompt('Input number of columns');
+  for(let i=0;i<noOfRows;i++)
+  {
+    let row = table.insertRow(i);
+    for(let j=0;j<noOfCols;j++)
+    {
+      let cols = row.insertCell(j);
+      cols.textContent = `Row-${i} Column-${j}`;
+    }
+  }
 }
